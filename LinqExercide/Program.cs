f197a604726fd4f3fee0c2f9ef2874738fd57afb
welshboy54 <37490748+welshboy54@users.noreply.github.com>
2019-11-14 09:49:23 -0600

    Add project files.

---
+++ LinqExercide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqExercide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
